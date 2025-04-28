efbda2ad80c2b77378b278e8ba70b14dacf91ff0
Alec German <kimarimt@protonmail.com>
2025-04-28 08:23:25 -0400

    finished exercise 1.11

--- part1/unicafe/src/components/StatisticLine.jsx
+++ part1/unicafe/src/components/StatisticLine.jsx
@@ -1,4 +1,8 @@
-const StatisticLine = ({ text, value }) =>
-  <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 export default StatisticLine
--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -2,18 +2,20 @@ import StatisticLine from './StatisticLine'
 
 const Statistics = ({ good, bad, neutral }) => {
   const totalReviews = good + bad + neutral
-  const averageReviews = (good - bad) / totalReviews
-  const postiveRviews = good / totalReviews * 100
+  const averageReviews = ((good - bad) / totalReviews).toFixed(1)
+  const postiveReviews = (good / totalReviews * 100).toFixed(1)
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={totalReviews} />
-      <StatisticLine text="average" value={averageReviews} />
-      <StatisticLine text="positive" value={postiveRviews} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={totalReviews} />
+        <StatisticLine text="average" value={averageReviews} />
+        <StatisticLine text="positive" value={`${postiveReviews}%`} />
+      </tbody>
+    </table>
   )
 }
 
