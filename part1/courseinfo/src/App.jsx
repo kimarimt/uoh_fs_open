5f3aaf30ebd977bb853f31e81bf4e85fc7aaa760
Alec German <kimarimt@protonmail.com>
2025-04-28 02:31:51 -0400

    fixed formatting

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,6 @@
-import Content from "./components/Content"
-import Header from "./components/Header"
-import Total from "./components/Total"
+import Content from './components/Content'
+import Header from './components/Header'
+import Total from './components/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
