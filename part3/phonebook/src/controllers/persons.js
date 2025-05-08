8e9a853efd1122bc582bb1d1d289694ba2324768
Alec German <kimarimt@protonmail.com>
2025-05-08 05:32:04 -0400

    finished exercise 3.3

--- part3/phonebook/src/controllers/persons.js
+++ part3/phonebook/src/controllers/persons.js
@@ -2,31 +2,41 @@ import express from 'express'
 
 export const persons = [
   { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "730-880-2302"
+    'id': '1',
+    'name': 'Arto Hellas', 
+    'number': '730-880-2302'
   },
   { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "335-546-6753"
+    'id': '2',
+    'name': 'Ada Lovelace', 
+    'number': '335-546-6753'
   },
   { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "849-839-7702"
+    'id': '3',
+    'name': 'Dan Abramov', 
+    'number': '849-839-7702'
   },
   { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "843-288-9051"
+    'id': '4',
+    'name': 'Mary Poppendieck', 
+    'number': '843-288-9051'
   }
 ]
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.json(persons)
+})
+
+router.get('/:id', (req, res) => {
+  const person = persons.find(p => p.id === req.params.id)
+
+  if (!person) {
+    return res.status(404).json('Person not found')
+  }
+
+  res.json(person)
 })
 
 export default router
