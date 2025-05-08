72c717cfbaad92b0095160a956656449770f926f
Alec German <kimarimt@protonmail.com>
2025-05-08 11:32:46 -0400

    finished exercise 3.6

--- part3/phonebook/src/controllers/persons.js
+++ part3/phonebook/src/controllers/persons.js
@@ -1,24 +1,24 @@
 import express from 'express'
 
 export let persons = [
-  { 
+  {
     'id': '1',
-    'name': 'Arto Hellas', 
+    'name': 'Arto Hellas',
     'number': '730-880-2302'
   },
-  { 
+  {
     'id': '2',
-    'name': 'Ada Lovelace', 
+    'name': 'Ada Lovelace',
     'number': '335-546-6753'
   },
-  { 
+  {
     'id': '3',
-    'name': 'Dan Abramov', 
+    'name': 'Dan Abramov',
     'number': '849-839-7702'
   },
-  { 
+  {
     'id': '4',
-    'name': 'Mary Poppendieck', 
+    'name': 'Mary Poppendieck',
     'number': '843-288-9051'
   }
 ]
@@ -26,6 +26,20 @@ export let persons = [
 const router = express.Router()
 
 router.post('/', (req, res) => {
+  if (!req.body.name || !req.body.number) {
+    return res.status(400).json({ 
+      'error': 'name and number fields are required' 
+    })
+  }
+
+  const person = persons.find(p => p.name === req.body.name) 
+
+  if (person) {
+    return res.status(400).json({
+      'error': `name must be unique`
+    })
+  }
+
   const newPerson = {
     id: `${Math.floor(Math.random() * 96) + persons.length}`,
     ...req.body
@@ -43,7 +57,9 @@ router.get('/:id', (req, res) => {
   const person = persons.find(p => p.id === req.params.id)
 
   if (!person) {
-    return res.status(404).json('person not found')
+    return res.status(404).json({
+      'error': 'person not found'
+    })
   }
 
   res.json(person)
@@ -53,7 +69,9 @@ router.delete('/:id', (req, res) => {
   const person = persons.find(p => p.id === req.params.id)
 
   if (!person) {
-    return res.status(404).json('person not found')
+    return res.status(404).json({
+      'error': 'person not found'
+    })
   }
 
   persons = persons.filter(p => p.id !== req.params.id)
